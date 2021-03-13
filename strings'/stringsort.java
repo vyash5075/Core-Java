b0cba65cd580c21b1bf5d4e60a2eb2562c10b4f6
Yash Verma <vyash5075@gmail.com>
2021-03-14 00:50:06 +0530

    Update stringsort.java

--- strings'/stringsort.java
+++ strings'/stringsort.java
@@ -25,3 +25,48 @@ public class stringsort {
          System.out.print(sort(s));
     }
 }
+
+------------------------------------------------------
+    //way2   brute force
+    
+    
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package strings;
+import java.util.*;
+/**
+ *
+ * @author yash verma
+ */
+public class ques15 {
+    public static String sort(String s)
+    {
+        char arr[]=s.toCharArray();
+       
+        for(int i=0;i<arr.length;i++)
+        { 
+            for(int j=i;j<arr.length;j++)
+            {
+                if(arr[j]<arr[i])
+                {
+                    char temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;    
+                }
+            }
+            
+        }
+        String st=new String(arr);
+        return st;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter String");
+        String s=sc.nextLine();
+        System.out.print(sort(s));
+    }
+}
